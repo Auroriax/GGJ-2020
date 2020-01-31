8afa7bf796217cdd9715d6a1cb5c0c0075e731ea
DESKTOP-0LUJTLU\Ben <bengunnewijk@hotmail.com>
2020-01-31 22:25:32 +0100

    Created a color picker script which works on any renderer and a Gear material

---
+++ Assets/ColorPicker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode] [RequireComponent(typeof(Renderer))]
+public class ColorPicker : MonoBehaviour
+{
+    public Color selectedColor = Color.blue;
+    public Renderer rendererComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!rendererComponent)
+            rendererComponent = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rendererComponent.material.color = selectedColor;
+    }
+}
