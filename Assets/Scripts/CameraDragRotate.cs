7c4f6d78a601d4c3e973b808cc60fbc6ba5dae88
Amazingcookie <auroriax@gmail.com>
2020-01-31 21:01:20 +0100

    Simple camera that you can rotate around an indicated point (see CameraRotateTest scene for demonstration)

---
+++ Assets/Scripts/CameraDragRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDragRotate : MonoBehaviour
+{
+    public float speed = 1f;
+    public Vector3 target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("X: " + Input.GetAxis("Mouse X") + " Y: " + Input.GetAxis("Mouse Y"));
+            //transform.RotateAround(target, Vector3.right, -Input.GetAxis("Mouse Y") * speed * Time.deltaTime);
+            transform.RotateAround(target, Vector3.up, Input.GetAxis("Mouse X") * speed * Time.deltaTime);
+        }
+    }
+}
