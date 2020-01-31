66f8b8f769d37846f2952fba6c7d4f70c50fbc38
DESKTOP-0LUJTLU\Ben <bengunnewijk@hotmail.com>
2020-01-31 21:01:52 +0100

    Gear update with prefab colliders done

---
+++ Assets/TurnObjectArrowKeys.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnObjectArrowKeys : MonoBehaviour
+{
+    Rigidbody rb;
+    float torque = 0f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float turn = Input.GetAxis("Horizontal");
+        rb.AddTorque( transform.up * torque * turn);
+    }
+}
