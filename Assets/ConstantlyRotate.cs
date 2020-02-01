136280d0437ea15c4a08916e82e8cbd10c14f3fa
Amazingcookie <auroriax@gmail.com>
2020-02-01 13:12:43 +0100

    Added a number of stuff, including a test scene, an outline script from the asset store, and a way to swap specific objects with a manager for it. Removed some input manager entries

---
+++ Assets/ConstantlyRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ConstantlyRotate : MonoBehaviour
+{
+    public Rigidbody rb;
+    [Tooltip("Positive is clockwise, negative is counter-clockwise.")]
+    public float torque = 200f;
+
+    private void Reset()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddTorque( transform.up * torque);
+    }
+}
---
+++ Assets/SwapObjectManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapObjectManager : MonoBehaviour
+{
+    public GameObject CurrentlySelectedObject;
+
+    [HideInInspector]
+    public RaycastHit hit;
+    [HideInInspector]
+    public Ray ray;
+    [HideInInspector]
+    public bool raycastSuccess;
+
+    private void Reset()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        raycastSuccess = Physics.Raycast(ray, out hit, 100.0f);
+    }
+}
