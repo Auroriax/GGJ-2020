08f352384ed3bacd3392035fc5c40ac25f257288
Erik de Roos <software@erikderoos.nl>
2020-02-02 13:10:51 +0100

    Better tuning of movable bar controls.

--- Assets/Scripts/MovableBar.cs
+++ Assets/Scripts/MovableBar.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
 [RequireComponent(typeof(Rigidbody))]
 public class MovableBar : MonoBehaviour
 {
     public float ForceMultiplier = 0.005f;
-    
+    public MoveDirection MoveDirection = MoveDirection.AutoDetect;
+    public bool SwapForceDirection = false;
+    public bool SwapMouseAx = false;
+
     private Vector3 gameObjectSreenPoint;
     private Vector3 mousePreviousLocation;
     private Vector3 mouseCurLocation;
@@ -23,53 +24,46 @@ public class MovableBar : MonoBehaviour
         //This grabs the position of the object in the world and turns it into the position on the screen
         gameObjectSreenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
         //Sets the mouse pointers vector3
-        mousePreviousLocation = new Vector3(Input.mousePosition.x, gameObjectSreenPoint.y, Input.mousePosition.y);
+        mousePreviousLocation = GetMousePos();
     }
 
     void OnMouseDrag()
     {
-        mouseCurLocation = new Vector3(Input.mousePosition.x, gameObjectSreenPoint.y, Input.mousePosition.y);
-        var force = mouseCurLocation - mousePreviousLocation;//Changes the force to be applied
-        mousePreviousLocation = mouseCurLocation;
+        mouseCurLocation = GetMousePos();
 
+        var force = mouseCurLocation - mousePreviousLocation;
+        if (SwapForceDirection)
+            force = -force;
         rigidbody.velocity += force * ForceMultiplier;
+
+        mousePreviousLocation = mouseCurLocation;
+    }
+
+    public Vector3 GetMousePos()
+    {
+        var mouseX = Input.mousePosition.x;
+        var mouseY = Input.mousePosition.y;
+        if (SwapMouseAx)
+        {
+            var temp = mouseX;
+            mouseX = mouseY;
+            mouseY = temp;
+        }
+
+        if (MoveDirection == MoveDirection.X)
+            return new Vector3(mouseX, gameObjectSreenPoint.y, mouseY);
+        else if (MoveDirection == MoveDirection.Y)
+            return new Vector3(mouseY, mouseX, gameObjectSreenPoint.z);
+        else if (MoveDirection == MoveDirection.Z)
+            return new Vector3(gameObjectSreenPoint.x, mouseY, mouseX);
+        return new Vector3(mouseX, gameObjectSreenPoint.y, mouseY);
     }
 }
 
-//{  // Move with drag
-//    private Vector3 screenPoint;
-//    private Vector3 offset;
-//    private RigidbodyConstraints rigidbodyConstraints;
-
-//    private void Start()
-//    {
-//        rigidbodyConstraints = GetComponent<Rigidbody>().constraints;
-//    }
-
-//    void OnMouseDown()
-//    {
-//        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
-
-//        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-//    }
-
-//    void OnMouseDrag()
-//    {
-//        var curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-//        var curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-
-//        var x = transform.position.x;
-//        var y = transform.position.y;
-//        var z = transform.position.z;
-//        if ((rigidbodyConstraints & RigidbodyConstraints.FreezePositionX) == 0)
-//            x = curPosition.x;
-//        if ((rigidbodyConstraints & RigidbodyConstraints.FreezePositionY) == 0)
-//            y = curPosition.y;
-//        if ((rigidbodyConstraints & RigidbodyConstraints.FreezePositionZ) == 0)
-//            z = curPosition.z;
-
-//        transform.position = new Vector3(x, y, z);
-//    }
-//}
-
-
+public enum MoveDirection
+{
+    AutoDetect = 0,
+    X,
+    Y,
+    Z
+}
