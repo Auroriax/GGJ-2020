8e05c919e59124f75ab66be81575bf0c7bfbbff5
Amazingcookie <auroriax@gmail.com>
2020-02-02 12:43:22 +0100

    Bar sliders now have outline highlights upon mouse interaction

--- Assets/Scripts/MovableBar.cs
+++ Assets/Scripts/MovableBar.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Outline))]
 public class MovableBar : MonoBehaviour
 {
     public float ForceMultiplier = 0.005f;
@@ -12,10 +13,45 @@ public class MovableBar : MonoBehaviour
     private Vector3 mousePreviousLocation;
     private Vector3 mouseCurLocation;
     private new Rigidbody rigidbody;
+    private Outline outline;
+
+    private bool selected = false;
+    private bool hovered = false;
+
+    private float hoverWidth = 5f;
+    private float selectedWidth = 3f;
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        outline = GetComponent<Outline>();
+        outline.OutlineWidth = 0f;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (selected)
+        {
+            outline.OutlineWidth = selectedWidth;
+        }
+        else
+        {
+            outline.OutlineWidth = hoverWidth;
+        }
+        hovered = true;
+    }
+
+    private void OnMouseExit()
+    {
+        hovered = false;
+        if (!selected)
+        {
+            outline.OutlineWidth = 0f;
+        }
+        else
+        {
+            outline.OutlineWidth = selectedWidth;
+        }
     }
 
     void OnMouseDown()
@@ -24,6 +60,8 @@ public class MovableBar : MonoBehaviour
         gameObjectSreenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
         //Sets the mouse pointers vector3
         mousePreviousLocation = new Vector3(Input.mousePosition.x, gameObjectSreenPoint.y, Input.mousePosition.y);
+        outline.OutlineWidth = selectedWidth;
+        selected = true;
     }
 
     void OnMouseDrag()
@@ -33,6 +71,19 @@ public class MovableBar : MonoBehaviour
         mousePreviousLocation = mouseCurLocation;
 
         rigidbody.velocity += force * ForceMultiplier;
+    }
+
+    private void OnMouseUp()
+    {
+        selected = false;
+        if (hovered)
+        {
+            outline.OutlineWidth = hoverWidth;
+        }
+        else
+        {
+            outline.OutlineWidth = 0f;
+        }
     }
 }
 
